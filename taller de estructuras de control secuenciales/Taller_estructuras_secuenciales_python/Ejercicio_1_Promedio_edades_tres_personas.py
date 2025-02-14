d6e57b0d6a5052a27eca7d819c34371ca5cc52ea
KarenPiamba <karen.piambaa@uniagustiniana.edu.co>
2025-02-13 20:31:18 -0500

    Add files via upload

---
+++ taller de estructuras de control secuenciales/Taller_estructuras_secuenciales_python/Ejercicio_1_Promedio_edades_tres_personas.py
@@ -0,0 +1,8 @@
+#Entradas
+persona1=int(input("Ingrese edad persona1"))
+persona2=int(input("Ingrese edad persona2"))
+persona3=int(input("Ingrese edad persona3"))
+#Caja negra
+Total_promedio=((persona1+persona2+persona3)/3)
+#salida
+print("El promedio es",Total_promedio)
