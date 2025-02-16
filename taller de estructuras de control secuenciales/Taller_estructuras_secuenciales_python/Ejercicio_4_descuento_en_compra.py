727e233bc1f3e883da14efd838e4bf5d6c60b31a
KarenPiamba <karen.piambaa@uniagustiniana.edu.co>
2025-02-15 21:01:59 -0500

    Add files via upload

---
+++ taller de estructuras de control secuenciales/Taller_estructuras_secuenciales_python/Ejercicio_4_descuento_en_compra.py
@@ -0,0 +1,7 @@
+#Entrada
+Total_compra=float(input("Ingresa el valor total de la compra"))
+Descuento=Total_compra*0.15
+#Caja negra
+Total_final=(Total_compra-Descuento)
+#Salida
+print("Total a pagar con el descuento:", Total_final,)
