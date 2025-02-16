b088082d1873104b14bb3a638ca55f3101f7afa1
KarenPiamba <karen.piambaa@uniagustiniana.edu.co>
2025-02-15 20:33:46 -0500

    Add files via upload

---
+++ taller de estructuras de control secuenciales/Taller_estructuras_secuenciales_python/Ejercicio_2_Inversion_capital_en_banco.py
@@ -0,0 +1,8 @@
+#Entrada
+Capital_invertido=float(input("Ingrera el capital que invertiste"))
+Interes=0.02
+#Caja negra
+Ganancia=(Capital_invertido*0.02)
+Total_ganancia=(Ganancia+Capital_invertido)
+#Salida
+print("Ganancia en un mes:", Ganancia, "Total de la ganancia con el capital:",Total_ganancia,)
