548f65c4dc626b207d7766bef2d262811b40b60f
KarenPiamba <karen.piambaa@uniagustiniana.edu.co>
2025-02-19 00:23:10 -0500

    Add files via upload

---
+++ taller de estructuras de control secuenciales/Taller_estructuras_secuenciales_python/Ejercicio_3_vendedor.py
@@ -0,0 +1,7 @@
+#Entradas
+Sueldo_base=int(input("Ingresa el sueldo base:"))
+#Caja negra
+Comisiones=Sueldo_base*0.10
+Salario_neto=Sueldo_base+Comisiones
+#Salida
+print("El valor de sus comisiones es de:", Comisiones, "Su sueldo neto es de:",Salario_neto)
